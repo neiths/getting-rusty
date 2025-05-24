941e0051e16b2189db8fa6b798ed2513cae3d4c1
neiths <thienkute01022002@gmail.com>
2025-05-24 13:11:24 +0700

    day19: add main file to run service

--- day19/banking_system_api/src/main.rs
+++ day19/banking_system_api/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+mod models;
+mod auth;
+mod routes;
+
+use axum::{Router};
+use routes::{auth_routes, protected_routes};
+use db::init_db;
+
+#[tokio::main]
+async fn main() {
+    let pool = init_db().await;
+
+    let app = Router::new()
+        .merge(auth_routes(pool.clone()))
+        .merge(protected_routes(pool.clone()));
+
+    println!("🚀 Server running on http://localhost:3000");
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
