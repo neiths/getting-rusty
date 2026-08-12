4268c2f426c83ec6114dd87ff00a24073880351e
neiths <thienkute01022002@gmail.com>
2026-08-13 00:30:12 +0700

    add some exercises in flow control

---
+++ learning-2026/flow_control/src/flowcontrol_one.rs
@@ -0,0 +1,11 @@
+pub fn flowcontrol_one() {
+    let n = 5;
+
+    if n < 0 {
+        println!("{} is negative", n);
+    } else if n > 0 {
+        println!("{} is positive", n);
+    } else {
+        println!("{} is zero", n);
+    }
+}
--- learning-2026/rust-2048/src/main.rs
+++ learning-2026/rust-2048/src/main.rs
@@ -1,3 +1,52 @@
+extern crate serde;
+extern crate serde_json;
+extern crate rand;
+extern crate piston_window;
+extern crate opengl_graphics;
+extern crate sdl2_window;
+
+use piston_window::*;
+use sdl2_window::Sdl2Window;
+
+mod app;
+mod board;
+mod number_renderer;
+mod settings;
+mod tile;
+
 fn main() {
-    println!("Hello, world!");
+	use opengl_graphics::GlGraphics;	
+    let settings = settings::Settings::load();
+
+	let (width, height) = (settings.window_size[0], 
+	                       settings.window_size[1]);
+
+    // according to piston WindowSettings documentation, OpenGL::V3_2 is the default version
+    let mut window: PistonWindow<Sdl2Window> =
+        WindowSettings::new("Rust-2048", [width, height])
+            .exit_on_esc(true)
+            .build()
+            .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+
+    let mut app = app::App::new(&settings);
+
+    app.load();
+
+    let mut gl = GlGraphics::new(OpenGL::V3_2);
+
+    while let Some(e) = window.next() {
+        if let Some(ref args) = e.render_args() {
+            app.render(args, &mut gl);
+        }
+
+        if let Some(ref args) = e.update_args() {
+           // TODO: only update if necessary
+           // println!("update");
+           app.update(args);
+        }
+
+        if let Some(ref args) = e.press_args() {
+            app.key_press(args);
+        }
+    }
 }
