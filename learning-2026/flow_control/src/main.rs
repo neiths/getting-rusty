52832adade523720ab62aa2be01de15aba300705
neiths <thienkute01022002@gmail.com>
2026-08-16 23:03:36 +0700

    add more flow control rust exercies

--- learning-2026/flow_control/src/main.rs
+++ learning-2026/flow_control/src/main.rs
@@ -6,6 +6,8 @@ mod flowcontrol_seven;
 mod flowcontrol_six;
 mod flowcontrol_three;
 mod flowcontrol_two;
+mod flowcontrol_nine;
+mod flowcontrol_ten;
 
 use flowcontrol_eight::flowcontrol_eight;
 use flowcontrol_five::flowcontrol_five;
@@ -15,14 +17,29 @@ use flowcontrol_seven::flowcontrol_seven;
 use flowcontrol_six::flowcontrol_six;
 use flowcontrol_three::flowcontrol_three;
 use flowcontrol_two::flowcontrol_two;
+use flowcontrol_nine::flowcontrol_nine;
+use flowcontrol_ten::flowcontrol_ten;
 
 fn main() {
+    println!("1. ===============================");
     flowcontrol_one();
+    println!("2. ===============================");
     flowcontrol_two();
+    println!("3. ===============================");
     flowcontrol_three();
+    println!("4. ===============================");
     flowcontrol_four();
+    println!("5. ===============================");
     flowcontrol_five();
+    println!("6. ===============================");
     flowcontrol_six();
+    println!("7. ===============================");
     flowcontrol_seven();
+    println!("8. ===============================");
     flowcontrol_eight();
+    println!("9. ===============================");
+    flowcontrol_nine();
+    println!("10. ===============================");
+    flowcontrol_ten();
+
 }
