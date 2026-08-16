15c70e1973513c43a8bfdc36997a2647767839d3
neiths <thienkute01022002@gmail.com>
2026-08-16 18:37:00 +0700

    update flow control exercises with rust

---
+++ learning-2026/flow_control/src/flowcontrol_six.rs
@@ -0,0 +1,19 @@
+pub fn flowcontrol_six() {
+    let mut n = 1;
+
+    while n <= 10 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }
+
+    println!("n reaached {}, so loop is over", n);
+}
--- learning-2026/flow_control/src/main.rs
+++ learning-2026/flow_control/src/main.rs
@@ -1,12 +1,18 @@
+mod flowcontrol_eight;
 mod flowcontrol_five;
 mod flowcontrol_four;
 mod flowcontrol_one;
+mod flowcontrol_seven;
+mod flowcontrol_six;
 mod flowcontrol_three;
 mod flowcontrol_two;
 
+use flowcontrol_eight::flowcontrol_eight;
 use flowcontrol_five::flowcontrol_five;
 use flowcontrol_four::flowcontrol_four;
 use flowcontrol_one::flowcontrol_one;
+use flowcontrol_seven::flowcontrol_seven;
+use flowcontrol_six::flowcontrol_six;
 use flowcontrol_three::flowcontrol_three;
 use flowcontrol_two::flowcontrol_two;
 
@@ -16,4 +22,7 @@ fn main() {
     flowcontrol_three();
     flowcontrol_four();
     flowcontrol_five();
+    flowcontrol_six();
+    flowcontrol_seven();
+    flowcontrol_eight();
 }
