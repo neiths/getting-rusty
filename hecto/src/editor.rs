2c0fcc3bf92d46aa6fafb0623ae762a68a5bac89
neiths <thienkute01022002@gmail.com>
2026-08-25 23:17:44 +0700

    hecto: using struct KeyEvent, KeyCode from crossterm

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,5 +1,5 @@
+use crossterm::event::{Event::Key, KeyCode::Char, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
 
 pub struct Editor {}
 
@@ -10,20 +10,18 @@ impl Editor {
 
     pub fn run(&self) {
         enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
-                    if c == 'q' {
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{event:?} \r");
+                    if let Char(c) = event.code
+                        && c == 'q'
+                    {
                         break;
                     }
                 }
-                Err(err) => println!("Error: {}", err),
+                Err(err) => println!("Error: {err}"),
+                _ => (),
             }
         }
         disable_raw_mode().unwrap();
--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -3,6 +3,7 @@ mod editor;
 use editor::Editor;
 
 fn main() {
+    #![warn(clippy::all, clippy::pedantic)]
     let editor = Editor::default();
     editor.run();
 }
