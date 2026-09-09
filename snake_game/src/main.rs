47ebf62b44b09b41cd529601f8555f23e5f926c9
neiths <thienkute01022002@gmail.com>
2026-09-09 23:57:57 +0700

    make a big adjustment in snake game

---
+++ snake_game/src/main.rs
@@ -0,0 +1,149 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow;
+use graphics::rectangle::square;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::*;
+use piston::input::*;
+use piston::window::WindowSettings;
+
+use std::collections::LinkedList;
+use std::iter::FromIterator;
+
+struct Game {
+    gl: GlGraphics,
+    rows: u32,
+    cols: u32,
+    snake: Snake,
+    just_eaten: bool,
+    square_width: u32,
+    food: Food,
+    score: u32,
+}
+
+impl Game {
+    fn render(&mut self, arg: &RenderArgs) {
+        use graphics;
+
+        let green: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(arg.viewport(), |_c, gl| {
+            graphics::clear(green, gl);
+        });
+
+        self.snake.render(args);
+        self.food.render(&mut self.gl, args, self.square_width);
+    }
+
+    fn update(&mut self) {
+        self.snake.update();
+    }
+
+    fn pressed(&mut self, btn: &Button) {
+        let last_direction = self.snake.dir.clone();
+
+        self.snake.dir = match btn {
+            &Button::Keyboard(Key::Up) if last_direction != Direction::Down => Direction::Up,
+            &Button::Keyboard(Key::Down) if last_direction != Direction::Up => Direction::Down,
+            &Button::Keyboard(Key::Left) if last_direction != Direction::Right => Direction::Left,
+            &Button::Keyboard(Key::Right) if last_direction != Direction::Left => Direction::Right,
+            _ => last_direction,
+        }
+    }
+}
+
+struct Snake {
+    gl: GlGraphics,
+    snake_parts: LinkedList<(i32, i32)>,
+    width: u32,
+    d: Direction,
+}
+
+#[derive(Clone)]
+pub struct Snake_Piece(u32, u32);
+
+impl Snake {
+    pub fn render(&mut self, arg: &RenderArgs) {
+        use graphics;
+
+        let red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        let squares: Vec<graphics::types::Rectangle> = self
+            .snake_parts
+            .iter()
+            .map(|p| Snake_Piece(p.0 * self.width, p.1 * self.width))
+            .map(|p| graphics::rectangle::square(p.0 as f64, p.1 as f64, self.width as f64))
+            .collect();
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            squares
+                .into_iter()
+                .for_each(|square| graphics::rectangle(RED, square, transform, gl));
+        })
+    }
+
+    fn update(&mut self) {
+        let mut new_head = (*&self.body.front().expect("Snake has not body")).clone();
+        match self.dir {
+            Direction::Right => new_head.0 += 1,
+            Direction::Left => new_head.0 -= 1,
+            Direction::Up => new_head.1 -= 1,
+            Direction::Down => new_head.1 += 1,
+        }
+        self.body.push_front(new_head);
+        self.body.pop_back().unwrap();
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
+struct Food {
+    x: u32,
+    y: u32,
+}
+
+fn main() {
+    let opengl = OpenGL::V3_2;
+
+    let mut window: GlutinWindow = WindowSettings::new("Snake Game", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game {
+        gl: GlGraphics::new(opengl),
+        snake: Snake {
+            pos_x: 9,
+            pos_y: 9,
+            dir: Direction::Right,
+        },
+    };
+
+    let mut events = Events::new(EventSettings::new()).ups(8);
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+        }
+
+        if let Some(_) = e.update_args() {
+            game.update();
+        }
+
+        if let Some(k) = e.button_args() {
+            if k.state == ButtonState::Press {
+                game.pressed(&k.button);
+            }
+        }
+    }
+}
