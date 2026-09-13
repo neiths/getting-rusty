5ba2b450d29528ac6f20407b93088c8179fcf24b
neiths <thienkute01022002@gmail.com>
2026-09-13 23:24:07 +0700

    add short, long dead version for cat message

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,3 +1,5 @@
+use std::option;
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -5,16 +7,22 @@ struct Options {
     #[clap(default_value = "Meow!")]
     /// What does the cat say?
     message: String, // [1]
+
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
 }
 
 fn main() {
     let options = Options::parse(); // [2]
     let message = options.message;
 
+    let eye = if options.dead { "x" } else { "o" }; // 1
+
     println!("{}", message);
     println!(" \\");
     println!("  \\");
     println!("     /\\_/\\");
-    println!("    ( o o )");
+    println!("    ( {eye} {eye} )");
     println!("    =( I )=");
 }
