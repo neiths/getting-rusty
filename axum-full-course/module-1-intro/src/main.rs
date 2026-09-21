6ee9eb7681a8bf488ce65d17cfb6943d50c69c37
neiths <thienkute01022002@gmail.com>
2026-09-22 00:16:21 +0700

    add basic tokio handle async fns, listen tcp, and and router for axum

--- axum-full-course/module-1-intro/src/main.rs
+++ axum-full-course/module-1-intro/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    routing::{get, post},
+};
+
+async fn hello_world() -> &'static str {
+    "hello, World"
+}
+
+async fn hello_axum() -> String {
+    format!("Welcome to axum {}!", env! {"CARGO_PKG_VERSION"})
+}
+
+async fn health_check() -> &'static str {
+    "Ok"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/hello", get(hello_axum))
+        .route("/health", get(health_check));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to bind to port 3000");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Server failed to start");
 }
